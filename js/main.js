67ed95f1ee311509b118db3b7dd7ba6f752dd60a
Zual Rana <freelancerzual@gmail.com>
2024-12-26 19:56:43 +0600

    added gameover, life, score function

--- js/main.js
+++ js/main.js
@@ -1,4 +1,56 @@
 function playGames() {
-  hideElement("home");
-  showElement("playground");
+  // hide the element by adding hidden class
+  addClass("home", "hidden");
+  // show element by removing hidden class
+  removeClass("playground", "hidden");
+  continueGame();
+}
+
+function continueGame() {
+  const alphabet = getARandomAlphabet();
+  const showText = document.getElementById("show-text");
+  showText.innerText = alphabet;
+  // set background color
+  addClass(alphabet, "bg-yellow-400");
+}
+
+document.addEventListener("keyup", function (e) {
+  const playerPressed = e.key;
+  // close the game if the player press escape key
+  if (playerPressed === "Escape") {
+    gameOver();
+  }
+  const currentText = document.getElementById("show-text").innerText;
+  const expectedAlphabet = currentText.toLowerCase();
+  if (playerPressed === expectedAlphabet) {
+    const scoreValue = getCurrentValue("current-score");
+    let currentScoreValue = scoreValue + 1;
+    setValue("current-score", currentScoreValue);
+
+    removeClass(expectedAlphabet, "bg-yellow-400");
+    continueGame();
+  } else {
+    // get and set current life value
+    const lifeValue = getCurrentValue("current-life");
+    const currentLifeValue = lifeValue - 1;
+    setValue("current-life", currentLifeValue);
+
+    // hide playground and show final score
+    if (currentLifeValue === 0) {
+      gameOver();
+    }
+    console.log("you lost a life", currentLifeValue);
+  }
+});
+
+// play again
+function playAgain() {
+  addClass("score-section", "hidden");
+  removeClass("playground", "hidden");
+  setValue("current-life", 5);
+  const showText = document.getElementById("show-text").innerText;
+  removeClass(showText, "bg-yellow-400");
+  setValue("current-score", 0);
+
+  continueGame();
 }
--- js/utility.js
+++ js/utility.js
@@ -1,8 +1,35 @@
-function hideElement(elementId) {
+function addClass(elementId, className) {
   const element = document.getElementById(elementId);
-  element.classList.add("hidden");
+  element.classList.add(className);
 }
-function showElement(elementId) {
+
+function removeClass(elementId, className) {
   const element = document.getElementById(elementId);
-  element.classList.remove("hidden");
+  element.classList.remove(className);
+}
+
+function getARandomAlphabet() {
+  const alphabetText = "abcdefghijklmnopqrstuvwxyz";
+  const alphabet = alphabetText.split("");
+  const randomNumber = Math.floor(Math.random() * 26);
+  const randomAlphabet = alphabet[randomNumber];
+  return randomAlphabet;
+}
+function getCurrentValue(elementId) {
+  const currentText = document.getElementById(elementId);
+  const elementText = currentText.innerText;
+  const previewsValue = Number(elementText);
+  return previewsValue;
+}
+function setValue(elementId, value) {
+  const element = document.getElementById(elementId);
+  element.innerText = value;
+}
+function gameOver() {
+  addClass("playground", "hidden");
+  removeClass("score-section", "hidden");
+
+  // get the final result and set to final result
+  const scoreValue = getCurrentValue("current-score");
+  setValue("final-score", scoreValue);
 }
